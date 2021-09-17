62b5135f11ca95f1aadfffe787a924114e00533d
Souleyman <soulmandra@gmail.com>
2021-09-17 18:07:19 +0200

    VueJs

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+    <!-- <Button text="Update Task " color="yellow" />
+    <Button text="Delete Task " color="red" /> -->
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean
+  },
+  components: { Button }
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  /* color: blue; */
+}
+</style>
